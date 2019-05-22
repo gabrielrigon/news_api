8b353c9a419e46295c8dc9d9ec6653e32ac9e5c0
Gabriel Rigon <gabrielrigon@outlook.com>
2019-05-22 14:06:31 -0300

    fix(news): adjusts query helper

--- src/services/news/queryHelper.js
+++ src/services/news/queryHelper.js
@@ -4,14 +4,14 @@ const safeParams = paramNames => {
   const { services } = config
   const permitted = Object.entries(services || [])
 
-  if (!paramNames) {
+  if (!paramNames || paramNames === 'all') {
     return permitted
   }
 
   const sources = (paramNames || '').split(',') || []
-  
+
   return permitted.filter(item => {
-    const [key, _] = item 
+    const [key, _] = item
     return sources.includes(key)
   })
 }
